cc2498deb0f85ad1175b75ce8bc52e0912c97a64
GoudSagar <goud.sagar.t@gmail.com>
2023-07-31 22:02:48 +0530

    Update security-groups-rule.tf

--- terraform-work/security-groups-rule.tf
+++ terraform-work/security-groups-rule.tf
@@ -1,7 +1,7 @@
 resource "aws_security_group_rule" "inbound_ssh" {
   type            = "ingress"
-  from_port       = 22
-  to_port         = 22
+  from_port       = 80
+  to_port         = 80
   protocol        = "tcp"
   source_security_group_id = aws_security_group.webservers.id
 
