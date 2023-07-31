4e669575d605b8d2a7a81d6914c972e2c36c05cf
GoudSagar <goud.sagar.t@gmail.com>
2023-07-31 22:24:35 +0530

    Update install_httpd.sh

--- elb/install_httpd.sh
+++ elb/install_httpd.sh
@@ -1,6 +1,6 @@
-#! /bin/bash
-sudo yum update -y
-sudo yum install httpd -y
-sudo echo "<h1>DevOps World</h1>" | sudo tee /var/www/html/index.html
-sudo systemctl start httpd
-sudo systemctl enable httpd
+#!/bin/bash
+yum update -y
+yum install httpd -y
+echo " <h1> DevOps World </h1> " >  /var/www/html/index.html
+systemctl start httpd
+systemctl enable httpd
